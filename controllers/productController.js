3963d39446192053f7dc4635a0502b9f9b0248ab
hasti sutariya <nikenpatel8484@gmail.com>
2026-06-05 11:34:28 +0530

    edit and update product

--- controllers/productController.js
+++ controllers/productController.js
@@ -124,6 +124,43 @@ const updateProduct = async (req, res) => {
     }
 };
 
+const getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.status(200).json({
+            success: true,
+            message: "Products fetched successfully",
+            products,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+const getProductById = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found",
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: "Product fetched successfully",
+            product,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
 module.exports = {
-    addProduct, updateProduct
+    addProduct, updateProduct, getAllProducts, getProductById
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,11 +4,14 @@ const router = express.Router();
 const {
     addProduct,
     updateProduct,
+    getAllProducts,
+    getProductById
 } = require("../controllers/productController");
 const upload = require("../middleware/upload");
 
 router.post("/add-product", upload.array("images", 5), addProduct);
 router.put("/update-product/:id", updateProduct);
-
+router.get("/get-products", getAllProducts);
+router.get("/get-product/:id", getProductById);
 
 module.exports = router;
